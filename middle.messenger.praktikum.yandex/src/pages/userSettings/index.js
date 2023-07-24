676f7279a07b0a3014b2528f1a9efbeb1b5df61d
Solomatin Vasiliy <v.solomatin@i-teco.tu>
2023-07-24 22:02:54 +0300

    страницы через шаблонизатор

---
+++ middle.messenger.praktikum.yandex/src/pages/userSettings/index.js
@@ -0,0 +1,5 @@
+import registration from './userSettings.hbs';
+import './userSettings.css';
+const root = document.querySelector('#app');
+const userSettingsToRender = registration({regixtrationLabel: "Настройка пользователя", });
+if (window.location.pathname ==='/userSettings') {root.innerHTML = userSettingsToRender};
--- middle.messenger.praktikum.yandex/vite.config.js
+++ middle.messenger.praktikum.yandex/vite.config.js
@@ -1,18 +1,13 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import handlebars from 'vite-plugin-handlebars';
+import handlebars from './vite-plugin-handlebars';
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
-    outDir: resolve(__dirname,'dst')
+    outDir: resolve(__dirname,'dist'),
   },
   plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, 'src/partials'),
-      context: {
-        authorizationLabel: "Авторизация!"
-      }
-    }),
+    handlebars(),
   ],
 });
